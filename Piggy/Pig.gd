3abcdc2da25977e65f5605a30a701dd6b9557501
RMdS-GitHub <rmds.dev@protonmail.com>
2020-11-26 14:03:21 +0100

    Lesson 10. Added delta to the move function. Now xSpeed and Yspeed are multiplied by delta.This avoid time errors in realtime if our processor is overtasked. It also removes an error from the debugger. I changed the const SPEED into a export var so this one is available in the inspector. This is very cool to test the game in real time and make changements while testing.

--- Piggy/Pig.gd
+++ Piggy/Pig.gd
@@ -6,7 +6,8 @@ the player, and then move the pig thanks to function process.
 extends Area2D
 
 # Constant variable to be used within func _process, called by move(SPEED,SPEED)
-const SPEED = 4
+# I removed the const I put export so the var is available in Inspector.
+export(int) var SPEED = 50 
 
 """
 This function is used every frame. Gets input from the player and
@@ -14,24 +15,38 @@ then move the pig 4 px along the x or y axis.
 
 is_action_pressed: means the action is being held down.
 is_action_just_pressed: means action just barely pressed.
+
+We need to pass delta into the move function. If not debugger will tell us 
+that delta is never used in the function. We need to specify delta in case 
+the computer processor is overloaded. Delta the time since the previous 
+frame, in real time can be different for us if the previous case happens.
+Delta will track the time.
 """
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
-		move(SPEED, 0)
+		move(SPEED, 0, delta)
 	if Input.is_action_pressed("ui_left"):
-		move(-SPEED, 0)
+		move(-SPEED, 0, delta)
 	if Input.is_action_pressed("ui_up"):
-		move(0, -SPEED)
+		move(0, -SPEED, delta)
 	if Input.is_action_pressed("ui_down"):
-		move(0, SPEED)
+		move(0, SPEED, delta)
 		
 
 """
+move(x, y, delta).
 This function is used within function process. You call it with the parameters
 to move the pig Area2D along the x and y axis.
-move(x, y) 
+
+We need to pass delta into the move function. If not debugger will tell us 
+that delta is never used in the function. Delta the time since the previous 
+frame, in real time can be different for us if the previous case happens.
+Delta will track the time. We need to specify delta in case 
+the computer processor is overloaded.
+The xSpeed and ySpeed need to be multiplied by delta. This compensates how fast 
+our processor is calculating for our game.
 """	
-func move(xSpeed, ySpeed):
-	position.x += xSpeed
-	position.y += ySpeed
+func move(xSpeed, ySpeed, delta):
+	position.x += xSpeed * delta
+	position.y += ySpeed * delta
